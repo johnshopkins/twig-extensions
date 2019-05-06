b07a2f492cb35dbf9a18a0c4b7382a37f42aace4
Jen Wachter <jwachter@jhu.edu>
2019-05-06 12:17:01 -0400

    Modified GetImage extension to allow for media conditions

--- src/GetImage.php
+++ src/GetImage.php
@@ -6,6 +6,16 @@ class GetImage
 {
   protected $extension;
 
+  protected $defaults = [
+    // display size
+    'size' => 'medium_thumb',
+
+    // media conditions and the image size required by each condition (image sizes attribute)
+    'mediaConditions' => [],
+
+    'attr' => []
+  ];
+
   public function __construct()
   {
     $this->extension = new \Twig_SimpleFunction('getImage', [$this, 'getImage']);
@@ -16,9 +26,21 @@ class GetImage
     return $this->extension;
   }
 
-  public function getImage($image, $crop = 'thumbnail', $attr = [])
+  public function getImage($image, $options = [])
   {
+    if (empty($image)) {
+      return '';
+    }
+
+    $options = array_merge($this->defaults, $options);
+
     $id = is_object($image) ? $image->ID : $image['ID'];
-    return wp_get_attachment_image($id, $crop, false, $attr);
+
+    // add sizes
+    if (!empty($options['mediaConditions'])) {
+      $options['attr']['sizes'] = implode(', ', $options['mediaConditions']);
+    }
+
+    return wp_get_attachment_image($id, $options['size'], false, $options['attr']);
   }
 }
