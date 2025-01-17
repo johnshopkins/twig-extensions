619762b1b53798629de0f8f06c5fdf5356e21eb6
Jen Wachter <jwachter@jhu.edu>
2025-01-17 13:47:37 -0500

    Fixed bug

--- src/GetImage.php
+++ src/GetImage.php
@@ -39,16 +39,9 @@ class GetImage extends BaseExtension
     $options['attr']['srcset'] = wp_get_attachment_image_srcset($id, $options['size']);
 
     if (!empty($options['responsiveSizes'])) {
-
       $options['attr']['sizes'] = $this->responsiveImageHelper->getImageSizes($options['responsiveSizes']);
-
-    } else {
-      // default wordpress sizes
-      // ensures that even thumbnails have them (by default, they don't)
-      $options['attr']['sizes'] = wp_get_attachment_image_srcset($id, $options['size']);
     }
-
-
+    
     return wp_get_attachment_image($id, $options['size'], false, $options['attr']);
   }
 }
